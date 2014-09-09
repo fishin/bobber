c456a7ba7e73fa220fa4a8c8f02b42162bd0207d
lloydbenson <lloyd.benson@gmail.com>
2014-09-09 05:49:55 -0500

    lets get it started in here

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 bobber
 ======
+
+source control for ficion
+
+[![Build Status](https://secure.travis-ci.org/fishin/bobber.svg)](http://travis-ci.org/fishin/bobber)
---
+++ test/bobber.js
@@ -0,0 +1,49 @@
+var Fs = require('fs');
+var Hapi = require('hapi');
+var Lab = require('lab');
+var Bobber = require('../lib/bobber');
+var Path = require('path');
+
+var internals = {};
+
+var lab = exports.lab = Lab.script();
+var expect = Lab.expect;
+var before = lab.before;
+var after = lab.after;
+var describe = lab.describe;
+var it = lab.it;
+
+var bobberPath = '/tmp/bobber';
+
+describe('bobber', function () {
+
+    it('getCheckoutCommand new', function (done) {
+
+        Fs.mkdirSync(bobberPath);
+        var scm = { type: 'github',
+                    branch: 'origin/master',
+                    url: 'git@github.com:fishin/bobber'
+        };
+        var bobber = Bobber.getCheckoutCommand(bobberPath, scm);
+        Fs.rmdirSync(bobberPath);
+        expect(bobber).to.contain('git clone -b origin/master git@github.com:fishin/bobber');
+        done();
+    });
+
+    it('getCheckoutCommand existing', function (done) {
+
+        var gitPath = Path.join(bobberPath, '.git');
+        Fs.mkdirSync(bobberPath);
+        Fs.mkdirSync(gitPath);
+        var scm = { type: 'github',
+                    branch: 'origin/master',
+                    url: 'git@github.com:fishin/bobber'
+        };
+        var bobber = Bobber.getCheckoutCommand(bobberPath, scm);
+        Fs.rmdirSync(gitPath);
+        Fs.rmdirSync(bobberPath);
+        expect(bobber).to.contain('git pull origin/master master');
+        done();
+    });
+
+});
