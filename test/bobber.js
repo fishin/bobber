8f31fe7af1b0e9cba7ea182ef91cc460f4bd5358
lloydbenson <lloyd.benson@gmail.com>
2015-02-20 06:30:36 -0600

    start started on merge work

--- test/bobber.js
+++ test/bobber.js
@@ -16,7 +16,7 @@ var it = lab.it;
 
 var bobberPath = __dirname + '/tmp';
 
-internals.mockGithub = function (rateLimit, repo, callback) {
+internals.mockGithub = function (rateLimit, repo, merge, callback) {
 
     var server = new Hapi.Server();
     server.connection();
@@ -33,6 +33,20 @@ internals.mockGithub = function (rateLimit, repo, callback) {
             path: '/repos/org/' + repo + '/pulls',
             handler: {
                 file: __dirname + '/fixtures/pulls_' + repo + '.json'
+            }
+        },
+        {
+            method: 'GET',
+            path: '/repos/org/' + repo + '/pulls/14',
+            handler: {
+                file: __dirname + '/fixtures/pulls_' + repo + '_14.json'
+            }
+        },
+        {
+            method: 'PUT',
+            path: '/repos/org/' + repo + '/pulls/14/merge',
+            handler: {
+                file: __dirname + '/fixtures/pulls_' + repo + '_14_' + merge + '.json'
             }
         }
     ]);
@@ -145,7 +159,7 @@ describe('bobber', function () {
 
     it('getOpenPullRequests rate_limit https user', function (done) {
 
-        internals.mockGithub('rate_limit', 'repo', function (server) {
+        internals.mockGithub('rate_limit', 'repo', 'merge', function (server) {
 
             server.start(function() {
 
@@ -169,9 +183,59 @@ describe('bobber', function () {
         });
     });
 
+    it('getPullRequest', function (done) {
+
+        internals.mockGithub('rate_limit', 'repo', 'merge', function (server) {
+
+            server.start(function() {
+
+                var bobber = new Bobber({apiUrl: server.info.uri});
+                var scm = {
+                    url: 'https://github.com/org/repo'
+                };
+                bobber.getPullRequest(scm, 14, function(pr) {
+
+                    //console.log(pr);
+                    expect(pr.number).to.be.above(0);
+                    expect(pr.commit.length).to.equal(40);
+                    expect(pr.mergeCommit.length).to.equal(40);
+                    expect(pr.shortCommit.length).to.equal(7);
+                    expect(pr.repoUrl).to.equal('https://github.com/org/repo');
+                    server.stop();
+                    done();
+                });
+            });
+        });
+    });
+
+    it('mergePullRequest', function (done) {
+
+        internals.mockGithub('rate_limit', 'repo', 'merge', function (server) {
+
+            server.start(function() {
+
+                var bobber = new Bobber({apiUrl: server.info.uri});
+                var scm = {
+                    url: 'https://github.com/org/repo'
+                };
+                var token = 1;
+                var number = 14;
+                bobber.mergePullRequest(scm, number, token, function(result) {
+
+                    //console.log(result);
+                    expect(result.sha.length).to.equal(40);
+                    expect(result.merged).to.be.true();
+                    expect(result.message).to.equal('Pull Request successfully merged');
+                    server.stop();
+                    done();
+                });
+            });
+        });
+    });
+
     it('getOpenPullRequests rate_limit https', function (done) {
 
-        internals.mockGithub('rate_limit', 'repo', function (server) {
+        internals.mockGithub('rate_limit', 'repo', 'merge', function (server) {
 
             server.start(function() {
 
@@ -197,7 +261,7 @@ describe('bobber', function () {
 
     it('getOpenPullRequests rate_limit ssh', function (done) {
 
-        internals.mockGithub('rate_limit', 'repo', function (server) {
+        internals.mockGithub('rate_limit', 'repo', 'merge', function (server) {
 
             server.start(function() {
 
@@ -223,7 +287,7 @@ describe('bobber', function () {
 
     it('getOpenPullRequests rate_limit_reached', function (done) {
 
-        internals.mockGithub('rate_limit_reached', 'repo', function (server) {
+        internals.mockGithub('rate_limit_reached', 'repo', 'merge', function (server) {
 
             server.start(function() {
 
@@ -244,7 +308,7 @@ describe('bobber', function () {
 
     it('getOpenPullRequests invalid', function (done) {
 
-        internals.mockGithub('rate_limit', 'invalid', function (server) {
+        internals.mockGithub('rate_limit', 'invalid', 'merge', function (server) {
 
             server.start(function() {
 
