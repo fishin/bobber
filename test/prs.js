3b3607541743f2a7e17df50c8b44f8fa216692b4
lloydbenson <lloyd.benson@gmail.com>
2015-02-21 17:12:33 -0600

    use new Mock lib for the github api

--- test/prs.js
+++ test/prs.js
@@ -2,6 +2,7 @@ var Code = require('code');
 var Fs = require('fs');
 var Hapi = require('hapi');
 var Lab = require('lab');
+var Mock = require('mock');
 var Path = require('path');
 var Pail = require('pail');
 
@@ -14,60 +15,28 @@ var expect = Code.expect;
 var describe = lab.describe;
 var it = lab.it;
 
-var bobberPath = __dirname + '/tmp';
-
-internals.mockGithub = function (rateLimit, repo, merge, callback) {
-
-    var server = new Hapi.Server();
-    server.connection();
-    server.route([
-        {
-            method: 'GET',
-            path: '/rate_limit',
-            handler: {
-                file: __dirname + '/fixtures/' + rateLimit + '.json'
-            }
-        },
-        {
-            method: 'GET',
-            path: '/repos/org/' + repo + '/pulls',
-            handler: {
-                file: __dirname + '/fixtures/pulls_' + repo + '.json'
-            }
-        },
-        {
-            method: 'GET',
-            path: '/repos/org/' + repo + '/pulls/14',
-            handler: {
-                file: __dirname + '/fixtures/pulls_' + repo + '_14.json'
-            }
-        },
-        {
-            method: 'PUT',
-            path: '/repos/org/' + repo + '/pulls/14/merge',
-            handler: {
-                file: __dirname + '/fixtures/pulls_' + repo + '_14_' + merge + '.json'
-            }
-        },
-        {
-            method: 'POST',
-            path: '/repos/org/' + repo + '/statuses/1',
-            handler: {
-                file: __dirname + '/fixtures/repo_statuses_1.json'
-            }
-        }
-    ]);
-    callback(server);
-};
-
 describe('pull requests', function () {
 
     it('getPullRequests rate_limit https user notoken', function (done) {
 
-        internals.mockGithub('rate_limit', 'repo', 'merge', function (server) {
+        var type = 'github';
+        var routes = [
+            {
+                method: 'get',
+                path: '/repos/org/repo/pulls',
+                file: 'index.json'
+            },
+            {
+                method: 'get',
+                path: '/rate_limit',
+                file: 'anonymous.json'
+            }
+        ];
+        Mock.prepareServer(type, routes, function(server) {
 
             server.start(function() {
 
+                //console.log(server.info);
                 var bobber = new Bobber({apiUrl: server.info.uri});
                 var scm = {
                     url: 'https://anon:anon@github.com/org/repo'
@@ -81,7 +50,7 @@ describe('pull requests', function () {
                     expect(prs[0].mergeCommit.length).to.equal(40);
                     expect(prs[0].shortCommit.length).to.equal(7);
                     expect(prs[0].repoUrl).to.equal('https://anon:anon@github.com/org/repo');
-                    server.stop();
+                    server.stop;
                     done();
                 });
             });
@@ -90,10 +59,24 @@ describe('pull requests', function () {
 
     it('getPullRequests rate_limit https user token', function (done) {
 
-        internals.mockGithub('rate_limit', 'repo', 'merge', function (server) {
+        var type = 'github';
+        var routes = [
+            {
+                method: 'get',
+                path: '/repos/org/repo/pulls',
+                file: 'index.json'
+            },
+            {
+                method: 'get',
+                path: '/rate_limit',
+                file: 'authorized.json'
+            }
+        ];
+        Mock.prepareServer(type, routes, function(server) {
 
             server.start(function() {
 
+                //console.log(server.info);
                 var bobber = new Bobber({apiUrl: server.info.uri});
                 var scm = {
                     url: 'https://anon:anon@github.com/org/repo'
@@ -108,7 +91,7 @@ describe('pull requests', function () {
                     expect(prs[0].mergeCommit.length).to.equal(40);
                     expect(prs[0].shortCommit.length).to.equal(7);
                     expect(prs[0].repoUrl).to.equal('https://anon:anon@github.com/org/repo');
-                    server.stop();
+                    server.stop;
                     done();
                 });
             });
@@ -117,7 +100,20 @@ describe('pull requests', function () {
 
     it('getPullRequest rate_limit notoken', function (done) {
 
-        internals.mockGithub('rate_limit', 'repo', 'merge', function (server) {
+        var type = 'github';
+        var routes = [
+            {
+                method: 'get',
+                path: '/repos/org/repo/pulls/1',
+                file: 'index.json'
+            },
+            {
+                method: 'get',
+                path: '/rate_limit',
+                file: 'anonymous.json'
+            }
+        ];
+        Mock.prepareServer(type, routes, function(server) {
 
             server.start(function() {
 
@@ -125,7 +121,8 @@ describe('pull requests', function () {
                 var scm = {
                     url: 'https://github.com/org/repo'
                 };
-                bobber.getPullRequest(scm, 14, null, function(pr) {
+                var number = 1;
+                bobber.getPullRequest(scm, number, null, function(pr) {
 
                     //console.log(pr);
                     expect(pr.number).to.be.above(0);
@@ -142,7 +139,20 @@ describe('pull requests', function () {
 
     it('getPullRequest rate_limit_reached notoken', function (done) {
 
-        internals.mockGithub('rate_limit_reached', 'repo', 'merge', function (server) {
+        var type = 'github';
+        var routes = [
+            {
+                method: 'get',
+                path: '/repos/org/repo/pulls/1',
+                file: 'index.json'
+            },
+            {
+                method: 'get',
+                path: '/rate_limit',
+                file: 'reached.json'
+            }
+        ];
+        Mock.prepareServer(type, routes, function(server) {
 
             server.start(function() {
 
@@ -150,7 +160,8 @@ describe('pull requests', function () {
                 var scm = {
                     url: 'https://github.com/org/repo'
                 };
-                bobber.getPullRequest(scm, 14, null, function(pr) {
+                var number = 1;
+                bobber.getPullRequest(scm, number, null, function(pr) {
 
                     //console.log(pr);
                     expect(pr).to.not.exist();
@@ -163,7 +174,20 @@ describe('pull requests', function () {
 
     it('getPullRequest rate_limit token', function (done) {
 
-        internals.mockGithub('rate_limit', 'repo', 'merge', function (server) {
+        var type = 'github';
+        var routes = [
+            {
+                method: 'get',
+                path: '/repos/org/repo/pulls/1',
+                file: 'index.json'
+            },
+            {
+                method: 'get',
+                path: '/rate_limit',
+                file: 'authorized.json'
+            }
+        ];
+        Mock.prepareServer(type, routes, function(server) {
 
             server.start(function() {
 
@@ -172,7 +196,8 @@ describe('pull requests', function () {
                     url: 'https://github.com/org/repo'
                 };
                 var token = 1;
-                bobber.getPullRequest(scm, 14, token, function(pr) {
+                var number = 1;
+                bobber.getPullRequest(scm, number, token, function(pr) {
 
                     //console.log(pr);
                     expect(pr.number).to.be.above(0);
@@ -189,7 +214,20 @@ describe('pull requests', function () {
 
     it('updateCommitStatus rate_limit', function (done) {
 
-        internals.mockGithub('rate_limit', 'repo', 'merge', function (server) {
+        var type = 'github';
+        var routes = [
+            {
+                method: 'post',
+                path: '/repos/org/repo/statuses/1',
+                file: 'index.json'
+            },
+            {
+                method: 'get',
+                path: '/rate_limit',
+                file: 'authorized.json'
+            }
+        ];
+        Mock.prepareServer(type, routes, function(server) {
 
             server.start(function() {
 
@@ -214,7 +252,20 @@ describe('pull requests', function () {
 
     it('updateCommitStatus rate_limit_reached', function (done) {
 
-        internals.mockGithub('rate_limit_reached', 'repo', 'merge', function (server) {
+        var type = 'github';
+        var routes = [
+            {
+                method: 'post',
+                path: '/repos/org/repo/statuses/1',
+                file: 'index.json'
+            },
+            {
+                method: 'get',
+                path: '/rate_limit',
+                file: 'reached.json'
+            }
+        ];
+        Mock.prepareServer(type, routes, function(server) {
 
             server.start(function() {
 
@@ -238,7 +289,20 @@ describe('pull requests', function () {
 
     it('mergePullRequest merge rate_limit', function (done) {
 
-        internals.mockGithub('rate_limit', 'repo', 'merge', function (server) {
+        var type = 'github';
+        var routes = [
+            {
+                method: 'put',
+                path: '/repos/org/repo/pulls/1/merge',
+                file: 'index.json'
+            },
+            {
+                method: 'get',
+                path: '/rate_limit',
+                file: 'authorized.json'
+            }
+        ];
+        Mock.prepareServer(type, routes, function(server) {
 
             server.start(function() {
 
@@ -247,7 +311,7 @@ describe('pull requests', function () {
                     url: 'https://github.com/org/repo'
                 };
                 var token = 1;
-                var number = 14;
+                var number = 1;
                 bobber.mergePullRequest(scm, number, token, function(result) {
 
                     //console.log(result);
@@ -264,7 +328,20 @@ describe('pull requests', function () {
 
     it('mergePullRequest mergefail rate_limit', function (done) {
 
-        internals.mockGithub('rate_limit', 'repo', 'mergefail', function (server) {
+        var type = 'github';
+        var routes = [
+            {
+                method: 'put',
+                path: '/repos/org/repo/pulls/1/merge',
+                file: 'fail.json'
+            },
+            {
+                method: 'get',
+                path: '/rate_limit',
+                file: 'authorized.json'
+            }
+        ];
+        Mock.prepareServer(type, routes, function(server) {
 
             server.start(function() {
 
@@ -273,7 +350,7 @@ describe('pull requests', function () {
                     url: 'https://github.com/org/repo'
                 };
                 var token = 1;
-                var number = 14;
+                var number = 1;
                 bobber.mergePullRequest(scm, number, token, function(result) {
 
                     //console.log(result);
@@ -288,7 +365,20 @@ describe('pull requests', function () {
 
     it('mergePullRequest merge rate_limit_reached', function (done) {
 
-        internals.mockGithub('rate_limit_reached', 'repo', 'merge', function (server) {
+        var type = 'github';
+        var routes = [
+            {
+                method: 'put',
+                path: '/repos/org/repo/pulls/1/merge',
+                file: 'index.json'
+            },
+            {
+                method: 'get',
+                path: '/rate_limit',
+                file: 'reached.json'
+            }
+        ];
+        Mock.prepareServer(type, routes, function(server) {
 
             server.start(function() {
 
@@ -297,7 +387,7 @@ describe('pull requests', function () {
                     url: 'https://github.com/org/repo'
                 };
                 var token = 1;
-                var number = 14;
+                var number = 1;
                 bobber.mergePullRequest(scm, number, token, function(result) {
 
                     //console.log(result);
@@ -311,7 +401,20 @@ describe('pull requests', function () {
 
     it('getPullRequests rate_limit https', function (done) {
 
-        internals.mockGithub('rate_limit', 'repo', 'merge', function (server) {
+        var type = 'github';
+        var routes = [
+            {
+                method: 'get',
+                path: '/repos/org/repo/pulls',
+                file: 'index.json'
+            },
+            {
+                method: 'get',
+                path: '/rate_limit',
+                file: 'anonymous.json'
+            }
+        ];
+        Mock.prepareServer(type, routes, function(server) {
 
             server.start(function() {
 
@@ -337,7 +440,20 @@ describe('pull requests', function () {
 
     it('getPullRequests rate_limit ssh', function (done) {
 
-        internals.mockGithub('rate_limit', 'repo', 'merge', function (server) {
+        var type = 'github';
+        var routes = [
+            {
+                method: 'get',
+                path: '/repos/org/repo/pulls',
+                file: 'index.json'
+            },
+            {
+                method: 'get',
+                path: '/rate_limit',
+                file: 'anonymous.json'
+            }
+        ];
+        Mock.prepareServer(type, routes, function(server) {
 
             server.start(function() {
 
@@ -363,7 +479,20 @@ describe('pull requests', function () {
 
     it('getPullRequests rate_limit_reached', function (done) {
 
-        internals.mockGithub('rate_limit_reached', 'repo', 'merge', function (server) {
+        var type = 'github';
+        var routes = [
+            {
+                method: 'get',
+                path: '/repos/org/repo/pulls',
+                file: 'index.json'
+            },
+            {
+                method: 'get',
+                path: '/rate_limit',
+                file: 'reached.json'
+            }
+        ];
+        Mock.prepareServer(type, routes, function(server) {
 
             server.start(function() {
 
@@ -384,7 +513,20 @@ describe('pull requests', function () {
 
     it('getPullRequests invalid', function (done) {
 
-        internals.mockGithub('rate_limit', 'invalid', 'merge', function (server) {
+        var type = 'github';
+        var routes = [
+            {
+                method: 'get',
+                path: '/repos/org/invalid/pulls',
+                file: 'index.json'
+            },
+            {
+                method: 'get',
+                path: '/rate_limit',
+                file: 'anonymous.json'
+            }
+        ];
+        Mock.prepareServer(type, routes, function(server) {
 
             server.start(function() {
 
